29f0aaac0e23a2cb42c4e8a09e1db43a222fd6ca
weiyaoxi <weiyaoxi@sina.com>
2017-07-31 17:35:43 +0800

    添加缺失的文件

---
+++ mysql_tengxunyun/Mode.cs
@@ -0,0 +1,48 @@
+﻿namespace mysql_tengxunyun
+{
+    /// <summary>
+    /// COS对应 ： key：nba2k/user/<uid>,<regtime> value：<pwd>,<endtime>,<utime>,<sunum>,<unum>,<mcode>
+    /// </summary>
+    public class user
+    {
+        public string uid { get; set; }
+        public string regtime { get; set; }
+        public string pwd { get; set; }
+        public string endtime { get; set; }
+        public string utime { get; set; }
+        public string sunum { get; set; }
+        public string unum { get; set; }
+        public string mcode { get; set; }
+    }
+    /// <summary>
+    /// COS对应 ： key：nba2k/config/<uid> value：<txt>
+    /// </summary>
+    public class config
+    {
+        public string uid { get; set; }
+        public string txt { get; set; }
+    }
+    /// <summary>
+    /// COS对应 ： key：nba2k/log/<jtype>,<uid>,<jtime>,<num>,<msg> value：null
+    /// </summary>
+    public class log
+    {
+        public string type { get; set; }
+        public string uid { get; set; }
+        public string time { get; set; }
+        public string num { get; set; }
+        public string msg { get; set; }
+    }
+    /// <summary>
+    /// COS对应 ： key：nba2k/cami/<ukey>,<uid>,<type>,<num>,<utime> value：del<0 or 1>,<regtime>,<reguid>
+    /// </summary>
+    public class cami
+    {		
+        public string key { get; set; }
+        public string uid { get; set; }
+        public string type { get; set; }
+        public string num { get; set; }
+        public string utime { get; set; }
+
+    }
+}
---
+++ mysql_tengxunyun/nba2k.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mysql_tengxunyun
+{
+    public class nba2k
+    {
+        public static user GetUser(string uid)
+        {
+            user u = new user();
+            List<string> ls = new List<string>();
+            int ret = Cos.Get_B("<" + uid + ">", out ls);
+            if (ret == 200)
+            {
+                string ostr = "";
+                ret = Cos.Get(ls[0], out ostr);
+                if (ret == 200)
+                {
+                    string[] key = ls[0].Replace(@"nba2k/user/", "").Replace("<", "").Replace(">", "").Split(',');
+                    string[] value = ostr.Split(',');
+                    u.uid = uid;
+                    u.regtime = key[1];
+                    u.pwd = value[0];
+                    u.endtime = value[1];
+                    u.utime = value[2];
+                    u.sunum = value[3];
+                    u.unum = value[4];
+                    u.mcode = value[5];
+                }
+            }
+            return u;
+        }
+    }
+}
