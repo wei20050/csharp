e6a26e13e13f526579c9c06e2560b00b5dadabd8
魏尧曦 <weiyaoxi@sina.com>
2019-04-12 15:21:26 +0800

    yxz的调用示例

---
+++ yxzdemo/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Forms;
+using yxz;
+
+namespace demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //实例化钩子
+            var kh = new KeyHook();
+            //挂载钩子按键事件
+            kh.OnKeyPressEvent += OnKeyPress;
+            //注册钩子
+            kh.SetHook();
+        }
+
+        //按下按键时触发这个函数
+        private static void OnKeyPress(object o,KeyPressEventArgs e)
+        {
+            //判断按下的是 a
+            if (e.KeyChar == 'a')
+            {
+                //执行移动鼠标到  100,100 位置
+                Yx.MoveTo(100,100);
+            }
+        }
+    }
+}
