3decac13c90c9b35d98c5a975575ca8a4201ee6c
weiyaoxi <weiyaoxi@sina.com>
2019-06-21 09:45:26 +0800

    添加大漠实现 排除dotnet

---
+++ 排除DotNet实现大漠/dmDemoSrc/dmDemo/MainForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using yx;
+
+namespace dmDemo
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			//DmSoft.Ini();
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			DmSoft dm = new DmSoft();
+			dm.Delay(2000);
+			dm.KeyPressStr(".ddaccJJdAA23JKDJSK23dlaJdjlj666");
+		}
+	}
+}
